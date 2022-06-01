bcb711c6ce3c1f11bfa262591145570eae1d7b79
paolo-05 <Paolo.bianchessi05@gmail.com>
2022-06-01 12:33:25 +0200

    aggiunta la pagina -cittadinanza digitale-, il sito è finito

--- src/pages/Due.js
+++ src/pages/Due.js
@@ -1,3 +1,4 @@
+import { Image } from "react-bootstrap";
 import Footer from "../components/Footer";
 
 const Due = () => {
@@ -5,6 +6,219 @@ const Due = () => {
   return (
     <>
       <h1 className="top">{"Cittadinanza Digitale".toUpperCase()}</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://www.efficaciafiscale.com/wp-content/uploads/2017/07/societadipersone.jpg"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Il genere umano ha subìto molti cambiamenti e senza alcun
+                  dubbio l'avvento della modernità ha fatto da sfondo a queste
+                  mutazioni. La società è frutto delle azioni degli individui
+                  che la plasmano e modificano a loro piacimento e che si
+                  adattano alle situazioni che si verificano. Ci troviamo in
+                  un'epoca frenetica, dove tutto sembra mutare da un momento
+                  all'altro, dove non ci sono più riferimenti, sicurezze, punti
+                  d'incontro.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://freesvg.org/img/provider-internet.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Un'epoca dove non è più importante distinguersi dal gruppo ma
+                  omologarsi ad esso non solo perché magari si condividono
+                  determinati valori ma per paura di essere esclusi. Ogni
+                  individuo è un soggetto unico e irripetibile ma oggi sembra
+                  che tutti siano uguali non tanto nel modo di pensare o di
+                  condividere determinate ideologie, quanto nel modo di agire e
+                  di apparire.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Whatsapp_logo_svg.png/600px-Whatsapp_logo_svg.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Oggi si fa a gara a chi ha il cellulare più costoso, a chi
+                  viaggia di più, a chi è più alla moda. Il consumismo ha
+                  portato gli individui a guardare in un'unica direzione: quella
+                  del voler avere sempre di più anche magari se non ce lo si può
+                  permettere, pur di apparire agli occhi degli altri come
+                  perfetto, adatto per uno specifico ruolo.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/2048px-Facebook_f_logo_%282021%29.svg.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Un conto è far parte di una stessa categoria sociale, un conto
+                  è cercare di adeguarsi alla massa facendo cose che non ci
+                  appartengono. Di certo non sarà il cellulare da mille euro a
+                  dare valore ad una persona e a farla “entrare” in una cerchia
+                  sociale. Adattarsi alle attitudini di un gruppo per non essere
+                  e sentirsi esclusi provoca un effetto boomerang che torna
+                  indietro con molta più potenza di prima perché si rischia di
+                  annientare la reale essenza di quell'individuo.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/9f/Twitter_bird_logo_2012.svg/1200px-Twitter_bird_logo_2012.svg.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  La definizione di socialità va ben oltre i social network,
+                  creare e intrattenere relazioni con altre persone caratterizza
+                  l'essere social. Condividere foto o mettere like ai post degli
+                  amici non basta. Da sempre, la socialità si misura con
+                  l'attitudine a instaurare e perpetuare forme di interazione
+                  interpersonale. Essere presenti sui social non implica
+                  automaticamente l'essere social. Possiamo decidere di esserci
+                  senza condividere né interagire.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Viviamo in un'epoca in cui la predominanza dei social network
+                  nella nostra vita quotidiana è così forte che gran parte delle
+                  nostre azioni si interseca con le reti sociali online I social
+                  network sono spazi di connessione popolati da un gran numero
+                  di persone, con comportamenti e interessi specifici e
+                  tracciabili. Questo offre opportunità prima impensabili per le
+                  aziende o le persone che vogliono ottenere visibilità.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-sm">
+            <div
+              className="card"
+              style={{
+                background: "var(--main-color)",
+                width: "25rem",
+                textAlign: "left",
+                alignItems: "center",
+              }}
+            >
+              <Image
+                src="https://upload.wikimedia.org/wikipedia/commons/0/08/Pinterest-logo.png"
+                className="card-img-top"
+                style={{ width: "50%", height: "50%" }}
+              />
+
+              <div className="card-body">
+                <p className="card-text">
+                  Il governo cinese ha definito il “Piano per la costruzione di
+                  un Social Credit System” a cui, per ora, le persone aderiscono
+                  volontariamente. Diventerà obbligatorio nel 2020. Si dice che
+                  il fine sia costruire una 'cultura della sincerità' che
+                  somiglia più a un sistema di controllo dei cittadini. Un
+                  punteggio alto è indice di buona affidabilità. Solo i
+                  cittadini affidabili potranno beneficiare dei privilegi
+                  sociali concessi dal governo. In tal caso, impossibile parlare
+                  di libertà di scelta. Uno scenario non più distopico, ma
+                  sempre inquietante.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
